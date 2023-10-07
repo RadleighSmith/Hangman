a9ec56c6fef751ce7162f4972db3f5f6d08b3364
Radleigh Smith <135361802+RadleighSmith@users.noreply.github.com>
2023-10-07 09:41:05 +0000

    Add get_random_word function and imported random.

--- run.py
+++ run.py
@@ -1,21 +1,33 @@
-import gspread 
+import gspread
+import random
 from google.oauth2.service_account import Credentials
 
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
+def get_random_word():
+    """
+    Retrieves a random word from the Google Sheets.
+
+    Returns a random word in string format.
+    """
+
+    SCOPE = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive.file",
+        "https://www.googleapis.com/auth/drive"
     ]
 
-CREDS = Credentials.from_service_account_file(
-    'words.json',
-    scopes=SCOPE
-)
+    CREDS = Credentials.from_service_account_file(
+        'words.json',
+        scopes=SCOPE
+    )
 
-CLIENT = gspread.authorize(CREDS)
+    CLIENT = gspread.authorize(CREDS)
 
-SHEET = CLIENT.open('hangman_words').get_worksheet(0)
+    SHEET = CLIENT.open('hangman_words').sheet1
 
-words = SHEET.col_values(1)
+    words = SHEET.col_values(1)
 
-print(words)
+    random_word = random.choice(words)
+
+    return random_word
+
+print(get_random_word())
