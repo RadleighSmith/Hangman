1a2986becb86b672da7c3fda4b3f2dbd519f2052
Radleigh Smith <135361802+RadleighSmith@users.noreply.github.com>
2023-10-07 11:16:19 +0000

    Add initialize_game function

--- run.py
+++ run.py
@@ -30,4 +30,31 @@ def get_random_word():
 
     return random_word
 
-print(get_random_word())
+def initialize_game(difficulty):
+    """
+    Initializes the Hangman game.
+
+    Argument = The difficulty level (easy, medium, or hard) chosen by the user.
+
+    Returns A tuple containing the random word, the initial guessed word, and max_attempts based from the difficulty.
+    """
+    random_word = get_random_word()
+
+    # Initialize the guessed word with underscores
+    guessed_word = ['_'] * len(random_word)
+
+    max_attempts = 0
+
+    if difficulty == 'easy':
+        max_attempts = 6
+    elif difficulty == 'mediun':
+        max_attempts = 5
+    elif difficulty == 'hard':
+        max_attempts = 4
+
+    return random_word, guessed_word, max_attempts
+
+random_word, guessed_word, max_attempts = initialize_game('hard')
+print(random_word)
+print(' '.join(guessed_word))
+print(max_attempts)
